85a9c82eeeb2c2fd59bb6ee448cc6d5e7dedaaae
Kevin Munoz <munozk341@gmail.com>
2020-07-11 16:17:32 -0700

    added quicksort

---
+++ quicksort.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+class quicksort{
+
+    static void quickSort(int[] array, int left, int right){
+
+        if(left < right){
+            int index = partition(array, left, right);
+            quickSort(array, left, index);
+            quickSort(array, index + 1, right);
+        }
+    }
+
+    static int partition(int[] array, int left, int right){
+        int pivot = array[(left + right)/2];
+        while(left < right){
+            while(array[left] < pivot) left++;
+            while(array[right]>pivot) right--;
+            if(left<=right){
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+
+    static void swap(int[] array, int left, int right){
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+
+
+    public static void main(String[] args){
+        int[] array = new int[args.length];
+        for(int i = 0; i<args.length; i++){
+            array[i] = Integer.parseInt(args[i]);
+        }
+
+        quickSort(array, 0, array.length - 1);
+    }
+}
